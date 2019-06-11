09958a40ab39b69fc80d8bb5d2d19c5f2b261be5
Sherry Ummen <sherry.ummen@napa.fi>
2019-06-11 13:52:13 +0300

    MongoDBDefaults Start port to be public, so that it could be set

--- src/Mongo2Go/MongoDbDefaults.cs
+++ src/Mongo2Go/MongoDbDefaults.cs
@@ -3,7 +3,7 @@
     public static class MongoDbDefaults
     {
         public const string ProcessName = "mongod";
-        
+
         public const string MongodExecutable = "mongod";
 
         public const string MongoExportExecutable = "mongoexport";
@@ -13,7 +13,7 @@
         public const int DefaultPort = 27017;
 
         // but we don't want to get in trouble with productive systems
-        public const int TestStartPort = 27018;
+        public static int TestStartPort { get; set; } = 27018;
 
         public const string Lockfile = "mongod.lock";
     }
